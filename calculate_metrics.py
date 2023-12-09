0f9ef5ada9d306df571c27d18de199f85b312c9f
Maxim Zapolskii <eliornnn@gmail.com>
2023-12-09 23:52:58 +0300

    update

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -1,3 +1,5 @@
+import sys
+
 from matplotlib import pyplot
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification
@@ -42,7 +44,7 @@ def calculate_metrics(model,
 
     test_samples = generate_samples(model_ft, tokenizer_ft, device, size=300)
     test_reward = evaluate_samples(model_reward, tokenizer_reward, test_samples, device)
-    torch.tensor(test_reward).mean()
+    print(f"avg reward:{torch.tensor(test_reward).mean()}")
 
     del model_ft
     gc.collect()
@@ -60,3 +62,36 @@ def calculate_metrics(model,
     image_path = f"images/{image_name}"
     os.makedirs(os.path.dirname(image_path), exist_ok=True)
     pyplot.savefig(image_path)
+
+if __name__ == '__main__':
+    loss = sys.argv[1]
+    from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification
+    import torch
+    import pickle
+
+    max_length = 128
+
+    bert_tokenizer = AutoTokenizer.from_pretrained("lvwerra/distilbert-imdb", padding='max_length', truncation=True,
+                                                   max_length=max_length)
+    bert_model = AutoModelForSequenceClassification.from_pretrained("lvwerra/distilbert-imdb")
+    tokenizer = AutoTokenizer.from_pretrained("lvwerra/gpt2-imdb", padding='max_length', truncation=True,
+                                              max_length=max_length)
+    model = AutoModelForCausalLM.from_pretrained("lvwerra/gpt2-imdb")
+    model_fine_tuned = AutoModelForCausalLM.from_pretrained(f"models/{loss}")
+    model_fine_tuned.cuda()
+
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    model = model.to(device)
+    bert_model = bert_model.to(device)
+
+    calculate_metrics(model,
+                      tokenizer,
+                      model_fine_tuned,
+                      tokenizer,
+                      bert_model,
+                      bert_tokenizer)
+
